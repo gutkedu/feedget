e4b6b2aa3811e8c4b74bc0f659965c4a891a7e8b
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2022-05-04 17:19:39 -0300

    feat: feedback route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,48 +1,10 @@
 import express from "express";
-import { prisma } from "./prisma";
-import nodemailer from "nodemailer";
+import { routes } from "./routes";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json("Hello World");
-});
-
-const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "593318f912ee82",
-    pass: "2449ae2accd0f8",
-  },
-});
-
-app.post("/feedbacks", async (req, res) => {
-  const { type, comment, screenshot } = req.body;
-
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot,
-    },
-  });
-
-  await transport.sendMail({
-    from: "Equipe Feedget <oi@feedget.com>",
-    to: "Eduardo <eduardo@feedget.com>",
-    subject: "Novo Feedback",
-    html: [
-      `<div style="font-family: sans-serif; font-size:16px; color: #111;">`,
-      `<p>Tipo do feedback: ${type}</p>`,
-      `<p>Comentário: ${comment}</p>`,
-      `</div`,
-    ].join("\n"),
-  });
-
-  return res.status(201).json({ data: feedback });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log("HTTP server running!");
